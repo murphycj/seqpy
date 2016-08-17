f8f2e0f8f184fc4d7a09df4a64efa24a2863ac8e
murphycj <murphy.charlesj@gmail.com>
2016-08-17 18:28:24 -0400

    added option to take absolute value in RNK file generation

--- bin/deseq2rnk.py
+++ bin/deseq2rnk.py
@@ -43,39 +43,22 @@ def create_rnk(args,data):
         if args.ranking == 'statistic':
             result_data['name'] = data.index
             result_data['rank'] = data['stat'].tolist()
-            #result_data = result_data[
-            #    (~pandas.isnull(result_data['name'])) &
-            #    (~pandas.isnull(result_data['rank'])) &
-            #    (~np.isinf(result_data['rank']))
-            #    ]
         elif args.ranking == 'logPvalSignFC':
             result_data['name'] = data.index
             result_data['rank'] = (-np.log2(data['pvalue'])*np.sign(data['log2FoldChange'])).tolist()
-            #result_data = result_data[
-            #    (~pandas.isnull(result_data['name'])) &
-            #    (~pandas.isnull(result_data['rank'])) &
-            #    (~np.isinf(result_data['rank']))
-            #    ]
         elif args.ranking == 'log2FC':
             result_data['name'] = data.index
             result_data['rank'] = data['log2FoldChange'].tolist()
-            #result_data = result_data[
-            #    (~pandas.isnull(result_data['name'])) &
-            #    (~pandas.isnull(result_data['rank'])) &
-            #    (~np.isinf(result_data['rank']))
-            #    ]
         else:
             result_data['name'] = data.index
             result_data['rank'] = (-np.log2(data['pvalue'])).tolist()
-            #result_data = result_data[
-            #    (~pandas.isnull(result_data['name'])) &
-            #    (~pandas.isnull(result_data['rank'])) &
-            #    (~np.isinf(result_data['rank']))
-            #    ]
 
 
     if args.reverseSign:
         result_data['rank'] = -1 * result_data['rank']
+
+    if args.abs:
+        result_data['rank'] = abs(result_data['rank'])
 
     result_data.to_csv(args.out,index=False,header=False, sep='\t')
 
@@ -107,7 +90,8 @@ def main(args):
 parser = argparse.ArgumentParser(description='Creates RNK file from DESeq2 or LimmaVoom output.')
 parser.add_argument('--infile',type=str,help='csv file containing deseq2 results',required=True)
 parser.add_argument('--ranking',type=str,help='Ranking statistic to use (statistic (default), logPvalSignFC, log2FC, logPval)',default='statistic',required=False)
-parser.add_argument('--limmavoom',action='store_true',help='Include if results are from limmavoom',required=False)
+parser.add_argument('--limmavoom',action='store_true',help='Include if results are from limmavoom (default false)',required=False,default=False)
+parser.add_argument('--abs',action='store_true',help='Take absolute value of ranking statistic (default false)',required=False,default=False)
 parser.add_argument('--reverseSign',action='store_true',help='Reverse the sign of the ranking metric',required=False,default=False)
 parser.add_argument('--out',type=str,help='Output prefix',required=True)
 args = parser.parse_args()
