35a2dbc06c84bc4a5042c1aee7e6ea68334b3ab0
murphycj <murphy.charlesj@gmail.com>
2016-08-18 03:24:28 -0400

    comments

--- bin/vcf_to_table.py
+++ bin/vcf_to_table.py
@@ -1,3 +1,10 @@
+"""
+Convert a vcf file annotated by SnpEff into a csv file that has one mutation
+per line (or multiple lines for that particular mutation in case its effect
+has varying context) and with samples as columns to indicate which samples
+have which mutations
+"""
+
 import vcf
 import argparse
 
@@ -17,11 +24,22 @@ def main(args):
         if 'ANN' not in v.INFO:
             continue
 
+        #get the total number of samples with the mutation
+
+        n=0
+        for s in v.samples:
+            if s.called:
+                n+=1
+
         for ann in v.INFO['ANN']:
+
+            #split the ANN fields and get predicted effect
+
             info = ann.split('|')
             if (not args.everything) and (info[1] not in args.filter_effects):
                 continue
 
+            #if has effect that is desired, print it
 
             fout.write(
                 str(v.CHROM) + ',' +
@@ -34,11 +52,9 @@ def main(args):
                 info[10] + ',' +
                 info[1]
             )
-            n=0
-            for s in v.samples:
-                if s.called:
-                    n+=1
+
             fout.write(',' + str(n))
+            
             for s in v.samples:
 
                 #if mutation is present, print the mutation frequency
