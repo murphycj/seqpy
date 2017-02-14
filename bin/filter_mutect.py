9e27aec341a1ccca852eb1a542dda75bd8cf97a9
Charles Murphy <murphy.charlesj@gmail.com>
2017-02-14 14:22:29 -0500

    More filter mutest features

--- bin/filter_mutect.py
+++ bin/filter_mutect.py
@@ -29,30 +29,141 @@ def main(args):
         if float(v.INFO['NLOD']) < args.nlod:
             continue
 
-        if hasattr(v.samples[0].data,'AD'):
-            AD = v.samples[0].data.AD
-            freq = float(v.samples[0].data.AF)
-            if AD[1] < args.AD or sum(AD) < args.min or freq < args.freq:
+        # get sample index
+
+        samples = map(lambda x: x.sample, v.samples)
+        n = 0
+        for i in samples:
+            if i == args.tumor:
+                tumor_index = n
+            if i == args.normal:
+                normal_index = n
+            n += 1
+
+        # check minimum coverage in normal
+
+        if hasattr(v.samples[normal_index].data, 'AD'):
+            AD = v.samples[normal_index].data.AD
+            if sum(AD) < args.min_normal:
                 continue
         else:
-            continue
+            print 'Site does not have AD attr'
+            print v
+
+        # check minimum coverage in tumor
+
+        if hasattr(v.samples[tumor_index].data, 'AD'):
+            AD = v.samples[tumor_index].data.AD
+            freq = float(v.samples[tumor_index].data.AF)
+            if AD[1] < args.AD or sum(AD) < args.min_tumor or freq < args.freq:
+                continue
+        else:
+            print 'Site does not have AD attr'
+            print v
 
         vcf_out.write_record(v)
     vcf_out.close()
 
+
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--vcf',type=str,help='VCf file',required=True)
-parser.add_argument('--AD',type=int,help='Min alt-supporting reads',required=False)
-parser.add_argument('--freq',type=float,help='Min frequency',required=False)
-parser.add_argument('--min',type=int,help='Min reads',required=False)
-parser.add_argument('--tlod',type=int,help='Min TLOD',required=False)
-parser.add_argument('--nlod',type=int,help='Min TLOD',required=False)
-parser.add_argument('--out',type=str,help='out vcf file',required=True)
-parser.add_argument('--homologous_mapping_event',action='store_true',help='Remove homologous_mapping_event',required=False)
-parser.add_argument('--clustered_events',action='store_true',help='Remove clustered_events',required=False)
-parser.add_argument('--triallelic_site',action='store_true',help='Remove triallelic_site',required=False)
-parser.add_argument('--alt_allele_in_normal',action='store_true',help='Remove alt_allele_in_normal',required=False)
-parser.add_argument('--multi_event_alt_allele_in_normal',action='store_true',help='Remove multi_event_alt_allele_in_normal',required=False)
+parser.add_argument(
+    '--vcf',
+    type=str,
+    help='VCf file',
+    required=True
+)
+parser.add_argument(
+    '--tumor',
+    type=str,
+    help='Tumor sample name (default TUMOR)',
+    default='TUMOR',
+    required=False
+)
+parser.add_argument(
+    '--normal',
+    type=str,
+    help='Tumor sample name (default NORMAL)',
+    default='NORMAL',
+    required=False
+)
+parser.add_argument(
+    '--AD',
+    type=int,
+    help='Min alt-supporting reads (default 4)',
+    default=4,
+    required=False
+)
+parser.add_argument(
+    '--freq',
+    type=float,
+    help='Min frequency (default 0.05)',
+    default=0.05,
+    required=True
+)
+parser.add_argument(
+    '--min_tumor',
+    type=int,
+    help='Min coverage in tumor (default 12)',
+    default=12,
+    required=False
+)
+parser.add_argument(
+    '--min_normal',
+    type=int,
+    help='Min coverage in normal (default 12)',
+    default=12,
+    required=False
+)
+parser.add_argument(
+    '--tlod',
+    type=int,
+    help='Min TLOD',
+    default=6.3,
+    required=False
+)
+parser.add_argument(
+    '--nlod',
+    type=int,
+    help='Min TLOD',
+    default=2.2,
+    required=False
+)
+parser.add_argument(
+    '--out',
+    type=str,
+    help='out vcf file',
+    required=True
+)
+parser.add_argument(
+    '--homologous_mapping_event',
+    action='store_true',
+    help='Remove homologous_mapping_event',
+    required=False
+)
+parser.add_argument(
+    '--clustered_events',
+    action='store_true',
+    help='Remove clustered_events',
+    required=False
+)
+parser.add_argument(
+    '--triallelic_site',
+    action='store_true',
+    help='Remove triallelic_site',
+    required=False
+)
+parser.add_argument(
+    '--alt_allele_in_normal',
+    action='store_true',
+    help='Remove alt_allele_in_normal',
+    required=False
+)
+parser.add_argument(
+    '--multi_event_alt_allele_in_normal',
+    action='store_true',
+    help='Remove multi_event_alt_allele_in_normal',
+    required=False
+)
 args = parser.parse_args()
 
 main(args=args)
