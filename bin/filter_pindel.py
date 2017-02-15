d7fc4637a2a6a16c9172419ca40d9941dcba9d65
Charles Murphy <murphy.charlesj@gmail.com>
2017-02-15 06:40:12 -0500

    New tool to filter pindel output vcf

---
+++ bin/filter_pindel.py
@@ -0,0 +1,112 @@
+"""
+Filter a VCF file output from pindel tumor-normal mutation calling
+
+"""
+
+import vcf
+import argparse
+
+
+def main(args):
+    vcf_in = vcf.Reader(open(args.vcf, 'r'))
+    vcf_out = vcf.Writer(open(args.out, 'w'), vcf_in)
+
+    for v in vcf_in:
+        keep = False
+
+        # get sample index
+
+        samples = map(lambda x: x.sample, v.samples)
+        n = 0
+        for i in samples:
+            if i == args.tumor:
+                tumor_index = n
+            if i == args.normal:
+                normal_index = n
+            n += 1
+
+        # check minimum coverage in normal
+
+        if hasattr(v.samples[normal_index].data, 'AD'):
+            AD = v.samples[normal_index].data.AD
+            if sum(AD) < args.min_normal:
+                continue
+        else:
+            print 'Site does not have AD attr'
+            print v
+
+        # check minimum coverage in tumor
+
+        if hasattr(v.samples[tumor_index].data, 'AD'):
+            AD = v.samples[tumor_index].data.AD
+            if sum(AD) < args.min_tumor:
+                continue
+            freq = AD[1] / float(sum(AD))
+            if AD[1] < args.AD or freq < args.freq:
+                continue
+        else:
+            print 'Site does not have AD attr'
+            print v
+
+        vcf_out.write_record(v)
+    vcf_out.close()
+
+
+parser = argparse.ArgumentParser(description='Filter Pindel output')
+parser.add_argument(
+    '--vcf',
+    type=str,
+    help='VCf file',
+    required=True
+)
+parser.add_argument(
+    '--tumor',
+    type=str,
+    help='Tumor sample name (default TUMOR)',
+    default='TUMOR',
+    required=False
+)
+parser.add_argument(
+    '--normal',
+    type=str,
+    help='Tumor sample name (default NORMAL)',
+    default='NORMAL',
+    required=False
+)
+parser.add_argument(
+    '--AD',
+    type=int,
+    help='Min mutant-supporting reads in tumor (default 4)',
+    default=4,
+    required=False
+)
+parser.add_argument(
+    '--freq',
+    type=float,
+    help='Min frequency (default 0.05)',
+    default=0.05,
+    required=True
+)
+parser.add_argument(
+    '--min_tumor',
+    type=int,
+    help='Min coverage in tumor (default 12)',
+    default=12,
+    required=False
+)
+parser.add_argument(
+    '--min_normal',
+    type=int,
+    help='Min coverage in normal (default 12)',
+    default=12,
+    required=False
+)
+parser.add_argument(
+    '--out',
+    type=str,
+    help='out vcf file',
+    required=True
+)
+args = parser.parse_args()
+
+main(args=args)
