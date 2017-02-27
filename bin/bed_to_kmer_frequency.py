fa80be18da18fb50be537e129455adffcee542f7
Charles Murphy <murphy.charlesj@gmail.com>
2017-02-27 15:21:44 -0500

    Tool to count occurrences of kmers

---
+++ bin/bed_to_kmer_frequency.py
@@ -0,0 +1,97 @@
+import pyfaidx
+import pandas
+import argparse
+import os
+import sys
+import multiprocessing
+from itertools import islice, product
+
+
+def window(seq, n=2):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield ''.join(result)
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield ''.join(result)
+
+
+def main(args):
+
+    if not os.path.exists(args.fasta + '.fai'):
+        print '.fai file does not exist!'
+        sys.exit()
+
+    fasta = pyfaidx.Fasta(args.fasta)
+
+    kmers = {''.join(i): 0 for i in product('acgt', repeat=3)}
+
+    if args.bed is not None:
+        beds = []
+        fin = open(args.bed, 'r')
+        for line in fin.readlines():
+            line = line.rstrip().split('\t')
+            assert len(line) >= 3, "found less than 3 entries " + str(line)
+            beds.append(line)
+
+        beds = pandas.DataFrame(beds)
+
+        for group in beds.groupby(0):
+            chrom = group[0]
+            print chrom
+            chrom_seq = str(fasta[chrom]).lower()
+            for i in group[1].index:
+                start = int(group[1].ix[i][1]) - 1
+                end = int(group[1].ix[i][2]) - 1
+                seq = chrom_seq[start:end]
+
+                for kmer in kmers.keys():
+                    kmers[kmer] += seq.count(kmer)
+
+    else:
+        for chrom in fasta.keys():
+            print chrom
+            chrom = str(fasta[chrom]).lower()
+            for kmer in kmers.keys():
+                kmers[kmer] += chrom.count(kmer)
+
+    fout = open(args.out, 'w')
+    for i in kmers.keys():
+        fout.write(i + ',' + str(kmers[i]) + '\n')
+    fout.close()
+
+
+parser = argparse.ArgumentParser(
+    description='Compute tri-nucleotide frequency from fasta.'
+)
+parser.add_argument(
+    '--fasta',
+    type=str,
+    required=True,
+    help='Fasta file.'
+)
+parser.add_argument(
+    '--kmer',
+    type=int,
+    required=False,
+    help='k-mer length (default 3)'
+)
+parser.add_argument(
+    '--bed',
+    type=str,
+    required=False,
+    default=None,
+    help='(Optional) BED file to restrict the analysis to.'
+)
+parser.add_argument(
+    '--out',
+    type=str,
+    required=True,
+    help='Output file name'
+)
+args = parser.parse_args()
+
+main(args=args)
--- bin/count_sequenced_contexts.py
+++ bin/count_sequenced_contexts.py
@@ -4,32 +4,32 @@ import sys
 
 
 SUBSTITUTIONS = {
-    'a>c':[],
-    'a>g':[],
-    'a>t':[],
-    'g>a':[],
-    'g>c':[],
-    'g>t':[]
+    'a>c': [],
+    'a>g': [],
+    'a>t': [],
+    'g>a': [],
+    'g>c': [],
+    'g>t': []
 }
 
 CONTEXTS = []
-for i in ['a','c','t','g']:
-    for j in ['a','c','t','g']:
-        CONTEXTS.append([i,j])
+for i in ['a', 'c', 't', 'g']:
+    for j in ['a', 'c', 't', 'g']:
+        CONTEXTS.append([i, j])
 
-#12 possible substitutions, which can be effectively reduced to 6
+# 12 possible substitutions, which can be effectively reduced to 6
 CONVERSIONS = {
-    'a':'a',
-    'g':'g',
-    't':'a',
-    'c':'g'
+    'a': 'a',
+    'g': 'g',
+    't': 'a',
+    'c': 'g'
 }
 
 
 def count_context(args):
 
     depth = 0
-    contexts = ['','','']
+    contexts = ['', '', '']
     three = 0
     bases = 0
 
@@ -37,10 +37,10 @@ def count_context(args):
     for i in CONTEXTS:
         for j in SUBSTITUTIONS.keys():
             context_counts.append(i[0] + j[0] + i[1])
-    context_counts = {i:0 for i in context_counts}
+    context_counts = {i: 0 for i in context_counts}
 
     for line in sys.stdin:
-        if line.find('mpileup')!=-1:
+        if line.find('mpileup') != -1:
             continue
         try:
             line = line.rstrip().split('\t')
@@ -50,7 +50,7 @@ def count_context(args):
             contexts[0] = contexts[1]
             contexts[1] = contexts[2]
 
-            if depth>=args.n and reference!='n':
+            if depth >= args.n and reference != 'n':
                 three += 1
                 contexts[2] = reference
             else:
@@ -66,15 +66,27 @@ def count_context(args):
         except:
             import pdb; pdb.set_trace()
 
-    fout = open(args.output,'w')
+    fout = open(args.output, 'w')
     fout.write('context,count\n')
     for i in context_counts.keys():
         fout.write(i + ',' + str(context_counts[i]) + '\n')
     fout.close()
 
-parser = argparse.ArgumentParser(description='Count the number of times a certain tri-nucleotide is sequenced')
-parser.add_argument('--n',type=int,help='Min depth (default 10)',required=True)
-parser.add_argument('--output',type=str,help='Output file name.',required=True)
+parser = argparse.ArgumentParser(
+    description='Count the number of times a certain tri-nucleotide is sequenced'
+)
+parser.add_argument(
+    '--n',
+    type=int,
+    help='Min depth (default 10)',
+    required=True
+)
+parser.add_argument(
+    '--output',
+    type=str,
+    help='Output file name.',
+    required=True
+)
 args = parser.parse_args()
 
 count_context(args=args)
